1cda85c2c5815133027655c660fbe053755b69be
Phuong BKA <phuongbka@Phuongs-MacBook-Pro.local>
2018-09-08 10:39:50 +0700

    add

---
+++ ShoppingFirebase/ios/Pods/Target Support Files/Pods-ShoppingFirebase-tvOS/Pods-ShoppingFirebase-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShoppingFirebase_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_ShoppingFirebase_tvOS
+@end
---
+++ ShoppingFirebase/ios/Pods/Target Support Files/Pods-ShoppingFirebaseTests/Pods-ShoppingFirebaseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShoppingFirebaseTests : NSObject
+@end
+@implementation PodsDummy_Pods_ShoppingFirebaseTests
+@end
